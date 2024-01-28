9bb726a11a6b78b12a7cefc328bb3500ac5c473f
yanakiv30 <yanakiv30@gmail.com>
2024-01-28 14:43:35 +0200

    StartScreen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = { questions: [], status: "loading" };
 //status can be:  loading,error,ready,active,finished
@@ -9,27 +12,30 @@ function reducer(state, action) {
   switch (action.type) {
     case "dataReceived":
       return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{questions, status}, dispatch] = useReducer(reducer, initialState);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => console.error("Error"));
+      .catch((err) => dispatch({type: "dataFailed"}));
   }, []);
 
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status==="loading" && <Loader />}
+        {status==="error" && <Error />}
+        {status==="ready" && <StartScreen />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,10 @@
+export default function StartScreen() {
+    return (
+        <div className="start">
+            <h2>Welcome to the React Quiz!</h2>
+            <h3>X question to test your React mastery</h3>
+            <button>Let's start</button>
+            
+        </div>
+    )
+}
